0432db80405ddd4d24921984660a8e54b6aa0684
phiskrzy <phineas.berdelis@icloud.com>
2022-04-06 10:28:05 +0200

    initial commit

---
+++ phineas_portfolio/frontend_react/src/constants/images.js
@@ -0,0 +1,76 @@
+import email2 from '../assets/email2.png';
+import mobile from '../assets/mobile.png';
+import api from '../assets/api.png';
+import cpp from '../assets/cpp.png';
+import css from '../assets/css.png';
+import figma from '../assets/figma.png';
+import flutter from '../assets/flutter.png';
+import git from '../assets/git.png';
+import graphql from '../assets/graphql.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import mu5 from '../assets/mu5.png';
+import node from '../assets/node.png';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+import typescript from '../assets/typescript.png';
+import vue from '../assets/vue.png';
+import csharp from '../assets/csharp.png';
+import java from '../assets/java.png';
+
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
+
+import circle from '../assets/circle.svg';
+import logo2 from '../assets/logo2.jpg';
+import heropicture3 from '../assets/heropicture3.jpg';
+
+import adidas from '../assets/adidas.png';
+import amazon from '../assets/amazon.png';
+import asus from '../assets/asus.png';
+import bolt from '../assets/bolt.png';
+import nb from '../assets/nb.png';
+import skype from '../assets/skype.png';
+import spotify from '../assets/spotify.png';
+
+export default {
+  email2,
+  mobile,
+  api,
+  cpp,
+  css,
+  figma,
+  flutter,
+  git,
+  graphql,
+  html,
+  javascript,
+  mu5,
+  node,
+  python,
+  react,
+  redux,
+  sass,
+  typescript,
+  vue,
+  about01,
+  about02,
+  about03,
+  about04,
+  circle,
+  logo2,
+  adidas,
+  amazon,
+  asus,
+  bolt,
+  nb,
+  skype,
+  spotify,
+  heropicture3,
+  csharp,
+  java,
+};
---
+++ phineas_portfolio/frontend_react/src/container/Footer/Footer.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+
+import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { client } from '../../client';
+import './Footer.scss';
+
+const Footer = () => {
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { username, email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    const contact = {
+      _type: 'contact',
+      name: formData.username,
+      email: formData.email,
+      message: formData.message,
+    };
+
+    client.create(contact)
+      .then(() => {
+        setLoading(false);
+        setIsFormSubmitted(true);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <h2 className="head-text">Take a <span>coffee</span> & chat with <span>me</span></h2>
+
+      <div className="app__footer-cards">
+        <div className="app__footer-card ">
+          <img src={images.email2} alt="email" />
+          <a href="mailto:phineas.berdelis@icloud.com" className="p-text">phineas.berdelis@icloud.com</a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="phone" />
+          <a href="tel:+41 75 412 54 09" className="p-text">+41 75 412 54 09</a>
+        </div>
+      </div>
+      {!isFormSubmitted ? (
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
+          </div>
+          <div className="app__flex">
+            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+          </div>
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Your Message"
+              value={message}
+              name="message"
+              onChange={handleChangeInput}
+            />
+          </div>
+          <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
+        </div>
+      ) : (
+        <div>
+          <h3 className="head-text">
+            Thank <span>you</span> for getting in <span>touch!</span>
+          </h3>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AppWrap(
+MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app__whitebg'
+);
